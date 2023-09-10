4e33349241f620c877a649e64a55c32669eede4a
Kagamine Haku <blomaway@gmail.com>
2023-09-11 01:09:26 +0700

    Add project files.

---
+++ ITRocketLaunch/UI.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ITRocketLaunch
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Enable = new System.Windows.Forms.Button();
+            this.Disable = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Enable
+            // 
+            this.Enable.Location = new System.Drawing.Point(64, 19);
+            this.Enable.Name = "Enable";
+            this.Enable.Size = new System.Drawing.Size(75, 23);
+            this.Enable.TabIndex = 0;
+            this.Enable.Text = "Enable";
+            this.Enable.UseVisualStyleBackColor = true;
+            this.Enable.Click += new System.EventHandler(this.Enable_Click);
+            // 
+            // Disable
+            // 
+            this.Disable.Location = new System.Drawing.Point(64, 59);
+            this.Disable.Name = "Disable";
+            this.Disable.Size = new System.Drawing.Size(75, 23);
+            this.Disable.TabIndex = 1;
+            this.Disable.Text = "Disable";
+            this.Disable.UseVisualStyleBackColor = true;
+            this.Disable.Click += new System.EventHandler(this.Disable_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Disable);
+            this.groupBox1.Controls.Add(this.Enable);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Desktop Computer/Network Icon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Gardevoir IT Control";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Enable;
+        private System.Windows.Forms.Button Disable;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ITRocketLaunch/UI.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITRocketLaunch
+{
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+
+        private void Enable_Click(object sender, EventArgs e)
+        {
+            ToggleDesktopIcons(true);
+        }
+
+        private void Disable_Click(object sender, EventArgs e)
+        {
+            ToggleDesktopIcons(false);
+        }
+
+        private void ToggleDesktopIcons(bool enableIcons)
+        {
+            // The Registry key paths for the icons
+            string computerIconPath = @"Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel";
+            string computerIconPath2 = @"Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu";
+            string networkIconPath = @"Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\ClassicStartMenu";
+            string networkIconPath2 = @"Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel";
+            // Set the value data to 0 to enable or 1 to disable
+            int valueData = enableIcons ? 0 : 1;
+
+            // Modify the Registry for Computer icon
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(computerIconPath, true))
+            {
+                if (key != null)
+                {
+                    key.SetValue("{20D04FE0-3AEA-1069-A2D8-08002B30309D}", valueData, RegistryValueKind.DWord);
+                }
+            }
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(computerIconPath2, true))
+            {
+                if (key != null)
+                {
+                    key.SetValue("{20D04FE0-3AEA-1069-A2D8-08002B30309D}", valueData, RegistryValueKind.DWord);
+                }
+            }
+
+            // Modify the Registry for Network icon
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(networkIconPath, true))
+            {
+                if (key != null)
+                {
+                    key.SetValue("{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}", valueData, RegistryValueKind.DWord);
+                }
+            }
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(networkIconPath2, true))
+            {
+                if (key != null)
+                {
+                    key.SetValue("{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}", valueData, RegistryValueKind.DWord);
+                }
+            }
+
+            // Inform the user about the changes
+            if (enableIcons)
+            {
+                MessageBox.Show("Computer and Network icons enabled.", "Icons Enabled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Computer and Network icons disabled.", "Icons Disabled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            RestartWindowsExplorer();
+        }
+
+        static void RestartWindowsExplorer()
+        {
+            Process[] explorerProcesses = Process.GetProcessesByName("explorer");
+
+            foreach (Process process in explorerProcesses)
+            {
+                process.Kill();
+            }
+        }
+    }
+}
